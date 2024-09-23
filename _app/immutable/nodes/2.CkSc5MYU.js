9db9888265d03b01e637948a80a14bef981b5255
Corey Tegeler <coreytegeler@gmail.com>
2024-09-23 17:06:51 -0400

    Updates

---
+++ _app/immutable/nodes/2.CkSc5MYU.js
@@ -0,0 +1,20 @@
+import{s as X,n as I}from"../chunks/scheduler.BvLojk_z.js";import{S as Y,i as Z,e as n,s as r,b as J,c as l,d as E,m as h,h as p,f as K,g as b,n as M,j as ee,k as e}from"../chunks/index.CaLM_tdh.js";import{b as Q}from"../chunks/paths.BzN4iykK.js";function te(W){let t,d,N="c2pa-ui/docs",g,a,u,j="c2pa-ui/svelte",y,v,A=`<script>
+	import { C2paUiSvelte } from "c2pa-ui/svelte";
+<\/script>
+
+<C2paUiSvelte
+	locale=""
+	src=""
+	caption=""
+	byline=""
+/>`,L,k,m,D='View the code and documenation on <a href="https://github.com/showyourworklab/c2pa-ui/tree/development/packages/svelte">GitHub</a>',w,f,G=`Check out the <a href="${Q}/../svelte">demo</a>`,z,s,C,R="c2pa-ui/react",S,_,V=`
+import { C2paUiReact } from "c2pa-ui/react";
+
+return (
+	<C2paUiReact
+		locale=""
+		src=""
+		caption=""
+		byline=""
+	/>
+)`,P,U,x,$='View the code and documenation on <a href="https://github.com/showyourworklab/c2pa-ui/tree/development/packages/react">GitHub</a>',O,H,q=`Check out the <a href="${Q}/../react">demo</a>`;return{c(){t=n("main"),d=n("h1"),d.textContent=N,g=r(),a=n("section"),u=n("h2"),u.textContent=j,y=r(),v=n("code"),L=J(A),k=r(),m=n("p"),m.innerHTML=D,w=r(),f=n("p"),f.innerHTML=G,z=r(),s=n("section"),C=n("h2"),C.textContent=R,S=r(),_=n("code"),P=J(V),U=r(),x=n("p"),x.innerHTML=$,O=r(),H=n("p"),H.innerHTML=q,this.h()},l(T){t=l(T,"MAIN",{class:!0});var o=E(t);d=l(o,"H1",{"data-svelte-h":!0}),h(d)!=="svelte-1seopfn"&&(d.textContent=N),g=p(o),a=l(o,"SECTION",{class:!0});var c=E(a);u=l(c,"H2",{"data-svelte-h":!0}),h(u)!=="svelte-m23s1h"&&(u.textContent=j),y=p(c),v=l(c,"CODE",{class:!0});var B=E(v);L=K(B,A),B.forEach(b),k=p(c),m=l(c,"P",{"data-svelte-h":!0}),h(m)!=="svelte-pwfpei"&&(m.innerHTML=D),w=p(c),f=l(c,"P",{"data-svelte-h":!0}),h(f)!=="svelte-1i7zvir"&&(f.innerHTML=G),c.forEach(b),z=p(o),s=l(o,"SECTION",{class:!0});var i=E(s);C=l(i,"H2",{"data-svelte-h":!0}),h(C)!=="svelte-1ycjd6f"&&(C.textContent=R),S=p(i),_=l(i,"CODE",{class:!0});var F=E(_);P=K(F,V),F.forEach(b),U=p(i),x=l(i,"P",{"data-svelte-h":!0}),h(x)!=="svelte-18nu1ay"&&(x.innerHTML=$),O=p(i),H=l(i,"P",{"data-svelte-h":!0}),h(H)!=="svelte-lnubdf"&&(H.innerHTML=q),i.forEach(b),o.forEach(b),this.h()},h(){M(v,"class","svelte-32z3hp"),M(a,"class","svelte-32z3hp"),M(_,"class","svelte-32z3hp"),M(s,"class","svelte-32z3hp"),M(t,"class","c2paUi-App svelte-32z3hp")},m(T,o){ee(T,t,o),e(t,d),e(t,g),e(t,a),e(a,u),e(a,y),e(a,v),e(v,L),e(a,k),e(a,m),e(a,w),e(a,f),e(t,z),e(t,s),e(s,C),e(s,S),e(s,_),e(_,P),e(s,U),e(s,x),e(s,O),e(s,H)},p:I,i:I,o:I,d(T){T&&b(t)}}}class le extends Y{constructor(t){super(),Z(this,t,null,te,X,{})}}export{le as component};
---
+++ svelte/_app/immutable/entry/start.DPBOtKD4.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.cqpDEaCe.js";export{t as start};
