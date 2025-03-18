72cbb4b736c989790808dfe1415e7b8d6b2e5f61
Corey Tegeler <coreytegeler@gmail.com>
2025-03-18 08:45:08 -0400

    Update svelte-preprocess import

--- packages/svelte/svelte.config.js
+++ packages/svelte/svelte.config.js
@@ -1,8 +1,8 @@
-import preprocess from 'svelte-preprocess';
+import { sveltePreprocess } from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
 const config = {
-	preprocess: preprocess(),
+	preprocess: sveltePreprocess(),
 	kit: {
 		alias: {
 			$src: './src',
