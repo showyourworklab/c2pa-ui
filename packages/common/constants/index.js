81d06c0260f552a8b5527d284128639d7bcaa588
Corey Tegeler <coreytegeler@gmail.com>
2025-12-16 09:53:43 -0500

    Update NO and SE copy

--- packages/common/constants/index.js
+++ packages/common/constants/index.js
@@ -38,8 +38,10 @@ export const DEMO_IMAGE_URLS = [
 	'leica-nora-7.jpg',
 	'leica-nora-8.jpg',
 	'leica-nora-9.jpg',
+	// 'leica-nora-test.jpg'
 ]
 
-export const DEMO_IMAGE_URL_DEFAULT = 'truepic-1.jpg'
+export const DEMO_IMAGE_URL_DEFAULT = 'leica-nora-1.jpg'
+// export const DEMO_IMAGE_URL_DEFAULT = 'leica-nora-test.jpg'
 
 export const DEMO_IMAGE_URL_BASE = 'https://showyourworklab.github.io/c2pa-images'
