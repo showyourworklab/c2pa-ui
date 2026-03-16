57087d54a12b28b04b8c59d6efdd55fda4248f5d
Corey Tegeler <coreytegeler@gmail.com>
2026-03-16 13:09:37 -0400

    Update demo.js

--- packages/common/constants/demo.js
+++ packages/common/constants/demo.js
@@ -24,4 +24,5 @@ export const DEMO_IMAGE_URLS = [
 ]
 
 // export const DEMO_IMAGE_URL_DEFAULT = 'https://showyourworklab.github.io/c2pa-images/leica-nora-11.jpg'
-export const DEMO_IMAGE_URL_DEFAULT = DEMO_IMAGE_URLS[DEMO_IMAGE_URLS.length - 1]
+// export const DEMO_IMAGE_URL_DEFAULT = DEMO_IMAGE_URLS[DEMO_IMAGE_URLS.length - 1]
+export const DEMO_IMAGE_URL_DEFAULT = DEMO_IMAGE_URLS[0]
